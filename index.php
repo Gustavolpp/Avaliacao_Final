05cdb452d2646d0e63d359f695ad94f8cf21926d
Gustavolpp <gustavolpp@gmail.com>
2022-04-03 21:56:07 -0300

    Atualização do href para PHP

--- index.php
+++ index.php
@@ -44,16 +44,16 @@
             <div class="collapse navbar-collapse" id="navbarNav">
               <ul class="navbar-nav">
                 <li class="nav-item active">
-                  <a class="nav-link" accesskey="1" href="index.html" title="Página incial">Home <span class="sr-only">(current)</span></a>
+                  <a class="nav-link" accesskey="1" href="index.php" title="Página incial">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" accesskey="2" href="formulario.html" title="Direcionar para o formulário de contatos">Formulário</a>
+                  <a class="nav-link" accesskey="2" href="formulario.php" title="Direcionar para o formulário de contatos">Formulário</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" accesskey="3" href="imagens.html" title="Direcionar para a página de imagens">Imagens</a>
+                  <a class="nav-link" accesskey="3" href="imagens.php" title="Direcionar para a página de imagens">Imagens</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" accesskey="4" href="sobre.html" title="Direcionar para a página considerações">Sobre</a>
+                  <a class="nav-link" accesskey="4" href="sobre.php" title="Direcionar para a página considerações">Sobre</a>
                 </li>
               </ul>
             </div>
